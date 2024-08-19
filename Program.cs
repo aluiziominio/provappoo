b1d972d4dc6282d2ce3defef0a9804bd78037ce0
Aluizio Teixeira <aluizio.teixeira@escolar.ifrn.edu.br>
2024-08-18 23:48:59 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int X = 10;         
+float Y = 1.2f;     
+string Z = "Olá";   
+bool W = true;      
+Console.WriteLine(X);
+Console.WriteLine(Y);
+Console.WriteLine(Z);
+Console.WriteLine(W);
